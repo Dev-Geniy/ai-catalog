b22b1742d5ba1233f32d89a6ab20612704a23219
Developer Evgeniy <79357491+Dev-Geniy@users.noreply.github.com>
2025-02-21 15:16:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,29 +8,3 @@
         document.body.removeChild(textArea);
         alert('Address copied!');
     }
-
-// Цвет вкладки
-    :root {
-        --theme-color: #ff007f;
-    }
-    
-    @keyframes colorChange {
-        0% { --theme-color: #ff007f; }
-        25% { --theme-color: #1e90ff; }
-        50% { --theme-color: #55f0ff; }
-        75% { --theme-color: #ffcc00; }
-        100% { --theme-color: #ff007f; }
-    }
-    
-    body {
-        animation: colorChange 8s infinite;
-    }
-</style>
-
-<script>
-    function updateThemeColor() {
-        let computedColor = getComputedStyle(document.documentElement).getPropertyValue('--theme-color').trim();
-        document.querySelector('meta[name="theme-color"]').setAttribute("content", computedColor);
-    }
-    
-    setInterval(updateThemeColor, 500);
